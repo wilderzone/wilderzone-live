3fd0a8d784e9f3a5abca4a114704a29c3986fd43
Giga <gigabyte5671@gmail.com>
2022-10-23 01:40:24 +1300

    Create SimpleButton component

---
+++ src/components/SimpleButton.vue
@@ -0,0 +1,36 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	name: 'SimpleButton',
+	props: {
+		color: {
+			type: String,
+			required: false
+		}
+	}
+});
+</script>
+
+<template>
+	<a class="simpleButton" :style="{ backgroundColor: $props.color }" href="">
+		<slot></slot>
+	</a>
+</template>
+
+<style lang="scss">
+.simpleButton {
+	display: flex;
+	flex-flow: row nowrap;
+	justify-content: center;
+	align-items: center;
+	gap: 1ch;
+	width: 20ch;
+	padding: 10px 15px;
+	color: #fff;
+	text-align: center;
+	text-decoration: none;
+	background-color: var(--primary_color_light);
+	border-radius: 7px;
+}
+</style>

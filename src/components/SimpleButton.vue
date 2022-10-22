e61f2cf6b634de81c2b3d6eb84640921ad524516
Giga <gigabyte5671@gmail.com>
2022-10-23 02:04:14 +1300

    Add a hover animation

--- src/components/SimpleButton.vue
+++ src/components/SimpleButton.vue
@@ -32,5 +32,41 @@ export default defineComponent({
 	text-decoration: none;
 	background-color: var(--primary_color_light);
 	border-radius: 7px;
+	box-shadow: 0px 2px 0px #888;
+
+	&::before,
+	&::after {
+		content: '';
+		position: absolute;
+		inset: 0;
+		display: block;
+		background: radial-gradient(
+			circle at center,
+			#fff 80%,
+			transparent calc(80% + 1px)
+		);
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: 0%;
+		opacity: 0;
+		transition: 0.2s ease all;
+		user-select: none;
+		pointer-events: none;
+	}
+
+	&::before {
+		transition-delay: 0.08s;
+	}
+
+	&:hover::before {
+		background-size: 105%;
+		opacity: 0.1;
+		transition-delay: 0.03s;
+	}
+
+	&:hover::after {
+		background-size: 90%;
+		opacity: 0.2;
+	}
 }
 </style>

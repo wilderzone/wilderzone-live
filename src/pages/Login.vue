6f7db145c0c30fd2ca600613825330f24350e072
Giga <gigabyte5671@gmail.com>
2022-10-23 21:10:28 +1300

    Create Login page

---
+++ src/pages/Login.vue
@@ -0,0 +1,35 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script lang="ts">
+import { defineComponent } from 'vue';
+import SiteHeader from '@/components/SiteHeader.vue';
+
+export default defineComponent({
+	name: 'Login',
+	components: {
+		SiteHeader
+	},
+	data() {
+		return {
+			username: '',
+			password: ''
+		};
+	}
+});
+</script>
+
+<template>
+	<SiteHeader :fixedHeading="true" />
+	<main>
+		<form action="post">
+			<label for="username">Username:</label>
+			<input type="text" name="username" v-model="username" />
+			<label for="password">Password:</label>
+			<input type="password" name="password" v-model="password" />
+			<button type="submit">Login</button>
+		</form>
+	</main>
+</template>
+
+<style lang="scss">
+@import '@/css/root.css';
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-	transpileDependencies: true
+	transpileDependencies: true,
+	pages: {
+		index: {
+			entry: 'src/main.ts',
+			template: 'public/index.html',
+			title: 'Home'
+		},
+		login: {
+			entry: 'src/pages/login.ts',
+			template: 'public/index.html',
+			title: 'Login'
+		}
+	}
 });
